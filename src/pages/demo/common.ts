ac00b27e60ff3fc1835ffde35f98fce0c004681c
duuganlx <3310888059@qq.com>
2023-09-11 17:49:26 +0800

    feat: 引入s2

---
+++ src/pages/demo/common.ts
@@ -0,0 +1,46 @@
+import dayjs from 'dayjs';
+
+/**
+ * author: lvxiang
+ * description: 存放一些通用的函数
+ */
+export interface CommonDataItem {
+  // 时间戳
+  timestamp10: number; // 10位时间戳
+  temperature: number; // 温度
+  temp10Thousand: number; // 温度(万倍)
+  temp100Million: number; // 温度(亿倍)
+  prevTemperature: number; // 前一个时间单位的温度
+  tempPnl: number; // 温度变化幅度 = (curTemp - prevTemp) / prevTemp
+}
+
+export function genCommonDataArray(size: number) {
+  const data: CommonDataItem[] = [];
+
+  let index = 0;
+  // HH:mm:ss => 23:59:59
+  let itime = dayjs().endOf('day').subtract(size, 'day');
+  let prevTemperature = 0;
+
+  while (index < size) {
+    const temperature = Math.random() * 10;
+
+    const tempPnl = (temperature - prevTemperature) / prevTemperature;
+
+    const tmp: CommonDataItem = {
+      timestamp10: itime.unix(),
+      temperature: temperature,
+      temp10Thousand: temperature * 10000,
+      temp100Million: temperature * 100000000,
+      prevTemperature: prevTemperature,
+      tempPnl: tempPnl,
+    };
+
+    itime = itime.add(1, 'day');
+    index++;
+    prevTemperature = temperature;
+    data.push(tmp);
+  }
+
+  return data;
+}
--- src/pages/demo/table/s2table/index.tsx
+++ src/pages/demo/table/s2table/index.tsx
@@ -1,5 +1,73 @@
+// import { S2DataConfig } from '@antv/s2';
+// import { SheetComponent } from '@antv/s2-react';
+import { useEmotionCss } from '@ant-design/use-emotion-css';
+import { Slider } from 'antd';
+import { useState } from 'react';
+
 const S2Table: React.FC = () => {
-  return <>s2table</>;
+  // const [widthPercentage, setWidthPercentage] = useState<number>(40);
+  const [heightPercentage, setHeightPercentage] = useState<number>(40);
+
+  // const data = genCommonDataArray(10);
+  // console.log(data);
+
+  // const s2DataCfg: S2DataConfig = {
+  //   data: data as any[],
+  //   fields: {
+  //     columns: [
+  //       'timestamp10',
+  //       'temperature',
+  //       'temp10Thousand',
+  //       'temp100Million',
+  //       'prevTemperature',
+  //       'tempPnl',
+  //     ],
+  //   },
+  //   meta: [
+  //     { field: 'timestamp10', name: '日期' },
+  //     { field: 'temperature', name: '温度(摄氏度)' },
+  //     { field: 'prevTemperature', name: '前一温度(摄氏度)' },
+  //     { field: 'temp10Thousand', name: '温度(万倍)' },
+  //     { field: 'temp100Million', name: '日期(亿倍)' },
+  //     { field: 'tempPnl', name: '变化幅度' },
+  //   ],
+  // };
+
+  const className = useEmotionCss(() => {
+    return {
+      // margin: '3px auto 0',
+      width: '80vw',
+      height: '30vh',
+      backgroundColor: 'white',
+      // border: '1px ',
+      // display: {},
+      // '.ctlPanel': {},
+    };
+  });
+
+  return (
+    <div className={className}>
+      <div className="display" style={{ width: '80vw', height: '10vh', backgroundColor: 'yellow' }}>
+        {/* <SheetComponent
+          sheetType="table"
+          adaptive={{ width: true, height: false }}
+          dataCfg={s2DataCfg}
+          options={{}}
+        /> */}
+      </div>
+
+      <div className="ctlPanel">
+        <Slider
+          min={1}
+          max={100}
+          onChange={(val: number) => {
+            setHeightPercentage(val);
+          }}
+          value={heightPercentage}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default S2Table;
